7937bcf71cc2221097499aa70fda8bf2e0f05a49
Herly Villamizar <55295596+herlyvilla@users.noreply.github.com>
2026-06-04 04:39:20 +0000

    cambios

--- idiomas.js
+++ idiomas.js
@@ -333,6 +333,19 @@ function changeLanguage(lang) {
     localStorage.setItem("language", lang);
 }
 
+const toggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("nav-menu");
+
+toggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+
+document.querySelectorAll("#nav-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("active");
+    });
+});
+
 document.getElementById("language-selector").addEventListener("change", function () {
     changeLanguage(this.value);
 });
